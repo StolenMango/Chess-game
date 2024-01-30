3c5c4f1d633e806da8fbdbbc4ceabd2aa1d726db
unknown <simionescuionut1@gmail.com>
2024-01-30 18:22:44 +0000

    Chessboard component created

--- chess-app/src/App.tsx
+++ chess-app/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Chessboard from "./components/Chessboard";
 
 function App() {
-  return <div>Chess board</div>;
+  return (
+    <div>
+      <Chessboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ chess-app/src/components/Chessboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Chessboard.css";
+
+export default function chessboard() {
+  return <div>This is the chessboard!</div>;
+}

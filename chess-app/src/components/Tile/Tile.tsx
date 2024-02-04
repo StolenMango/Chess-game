16168bb296753aed4d77241a8414d6e964790e76
unknown <simionescuionut1@gmail.com>
2024-02-04 03:29:52 +0000

    fixed class typo and reorganised assets folder, deleted some redundant files

--- chess-app/src/components/Tile/Tile.tsx
+++ chess-app/src/components/Tile/Tile.tsx
@@ -9,10 +9,10 @@ export default function Tile({ number }: Props) {
   if (number % 2 === 0) {
     return (
       <div className="axis-tile dark-tile">
-        <img src="assets/pawn_b.png" alt="pawn"></img>
+        S<img src="assets/pawn_b.png" alt="pawn"></img>
       </div>
     );
   } else {
-    return <div className="axis-tile bright-tile"></div>;
+    return <div className="axis-tile git addbright-tile"></div>;
   }
 }

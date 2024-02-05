8940c7a8206141e5f3a391999d7d50baed4871d0
unknown <simionescuionut1@gmail.com>
2024-02-05 17:17:24 +0000

    Initial board state

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -21,6 +21,24 @@ for (let i = 0; i < 8; i++) {
   pieces.push({ image: "assets/pawn_w.png", x: i, y: 1 });
 }
 
+pieces.push({ image: "assets/rook_b.png", x: 0, y: 7 });
+pieces.push({ image: "assets/rook_b.png", x: 7, y: 7 });
+pieces.push({ image: "assets/knight_b.png", x: 1, y: 7 });
+pieces.push({ image: "assets/knight_b.png", x: 6, y: 7 });
+pieces.push({ image: "assets/bishop_b.png", x: 2, y: 7 });
+pieces.push({ image: "assets/bishop_b.png", x: 5, y: 7 });
+pieces.push({ image: "assets/queen_b.png", x: 4, y: 7 });
+pieces.push({ image: "assets/king_b.png", x: 3, y: 7 });
+
+pieces.push({ image: "assets/rook_w.png", x: 0, y: 0 });
+pieces.push({ image: "assets/rook_w.png", x: 7, y: 0 });
+pieces.push({ image: "assets/knight_w.png", x: 1, y: 0 });
+pieces.push({ image: "assets/knight_w.png", x: 6, y: 0 });
+pieces.push({ image: "assets/bishop_w.png", x: 2, y: 0 });
+pieces.push({ image: "assets/bishop_w.png", x: 5, y: 0 });
+pieces.push({ image: "assets/queen_w.png", x: 4, y: 0 });
+pieces.push({ image: "assets/king_w.png", x: 3, y: 0 });
+
 pieces.push({ image: "assets/pawn_b.png", x: 0, y: 6 });
 //create 2 for loops to render the board, one loop within the other loop
 // don't forget to make a new array for it

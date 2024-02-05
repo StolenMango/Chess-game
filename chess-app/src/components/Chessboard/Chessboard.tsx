6085e7cec45e393f4f4556d1b08e68fe4f57721b
unknown <simionescuionut1@gmail.com>
2024-02-05 16:50:16 +0000

    piece placement logic

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -4,20 +4,31 @@ import "./Chessboard.css";
 // the 2 axis of the board
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+interface Piece {
+  image: string;
+  x: number;
+  y: number;
+}
+
+const pieces: Piece[] = [];
+
+pieces.push({ image: "assets/pawn_b.png", x: 0, y: 6 });
 //create 2 for loops to render the board, one loop within the other loop
 // don't forget to make a new array for it
 export default function chessboard() {
   let board = [];
   for (let l = verticalAxis.length - 1; l >= 0; l--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
-      //
       const number = i + l + 2; // vertical starts at 0 so we want to add 1, same for horizontal hence the +2
-      board.push(<Tile image="./assets/pawn_b.png" number={number}></Tile>);
-      // if (number % 2 === 0) {
-      //   board.push(<Tile />);
-      // } else {
-      //   board.push(<Tile />);
-      // }
+      let image = undefined;
+      //We'll need to check if there is a piece on any given tile.
+      pieces.forEach((p) => {
+        if (p.x === i && p.y === l) {
+          image = p.image;
+        }
+      }); // look throuygh each element without using a variable to directly access a piece element.
+      board.push(<Tile image={image} number={number}></Tile>);
     }
   }
   return <div id="chessboard">{board}</div>;
--- chess-app/src/components/Tile/Tile.tsx
+++ chess-app/src/components/Tile/Tile.tsx
@@ -10,13 +10,13 @@ export default function Tile({ number, image }: Props) {
   if (number % 2 === 0) {
     return (
       <div className="axis-tile dark-tile">
-        <img src={image} alt="chess piece"></img>
+        {image && <img src={image} alt="chess piece"></img>}
       </div>
     );
   } else {
     return (
       <div className="axis-tile bright-tile">
-        <img src={image} alt="chess piece"></img>
+        {image && <img src={image} alt="chess piece"></img>}
       </div>
     );
   }

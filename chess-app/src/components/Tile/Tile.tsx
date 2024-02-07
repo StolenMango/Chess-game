14ba3c108265899a2e71cbca741d9c5098ec71a3
unknown <simionescuionut1@gmail.com>
2024-02-07 22:37:35 +0000

    undefined chess piece rendering fixed

--- chess-app/src/components/Tile/Tile.tsx
+++ chess-app/src/components/Tile/Tile.tsx
@@ -10,13 +10,21 @@ export default function Tile({ number, image }: Props) {
   if (number % 2 === 0) {
     return (
       <div className="axis-tile dark-tile">
-        {image && <img src={image} alt="chess piece"></img>}
+        <div
+          className="chess-piece"
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+        {/* {image && <img src={image} alt="chess piece"></img>} */}
       </div>
     );
   } else {
     return (
       <div className="axis-tile bright-tile">
-        {image && <img src={image} alt="chess piece"></img>}
+        <div
+          className="chess-piece"
+          style={{ backgroundImage: `url(${image})` }} //this whole line is to fix being able to drag a picture into the browser
+        ></div>
+        {/* {image && <img src={image} alt="chess piece"></img>} */}
       </div>
     );
   }

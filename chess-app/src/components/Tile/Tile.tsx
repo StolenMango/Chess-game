fc2f565ec7a07e0ff41b91d643ed60bc2656cb49
unknown <simionescuionut1@gmail.com>
2024-02-08 16:32:25 +0000

    Ghost pieces fixed

--- chess-app/src/components/Tile/Tile.tsx
+++ chess-app/src/components/Tile/Tile.tsx
@@ -10,20 +10,24 @@ export default function Tile({ number, image }: Props) {
   if (number % 2 === 0) {
     return (
       <div className="axis-tile dark-tile">
-        <div
-          className="chess-piece"
-          style={{ backgroundImage: `url(${image})` }}
-        ></div>
+        {image && (
+          <div
+            className="chess-piece"
+            style={{ backgroundImage: `url(${image})` }}
+          ></div>
+        )}
         {/* {image && <img src={image} alt="chess piece"></img>} */}
       </div>
     );
   } else {
     return (
       <div className="axis-tile bright-tile">
-        <div
-          className="chess-piece"
-          style={{ backgroundImage: `url(${image})` }} //this whole line is to fix being able to drag a picture into the browser
-        ></div>
+        {image && (
+          <div
+            className="chess-piece"
+            style={{ backgroundImage: `url(${image})` }} //this whole line is to fix being able to drag a picture into the browser
+          ></div>
+        )}
         {/* {image && <img src={image} alt="chess piece"></img>} */}
       </div>
     );
